e9e80f0d6bd2ae0fe80d8dc3af61fb3f728c11d0
elribonazo <elribonazo@gmail.com>
2014-10-17 21:32:01 +0200

    get_album_tracks_by_artist & get_albums_by_artist

--- test/artist_albums.js
+++ test/artist_albums.js
@@ -2,9 +2,14 @@ var spm3 = require("../lib/spotify_m3");
 var async = require('async');
 
 
-spm3.load_artist_albums("coldplay", function(err, data){
+/*spm3.get_albums_by_artist("coldplay", function(err, data){
 	if(err) throw err;
-	
-	
-	console.log("DATA : " + data);
+	console.log("DATA : ");
+	console.log(data);
+});*/
+
+spm3.get_album_tracks_by_artist("coldplay", function(err, data){
+	if(err) throw err;
+	console.log("DATA : ");
+	console.log(data);
 });
