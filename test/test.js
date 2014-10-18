4e03cfdc0400d9fcdc139118796fd44ee4a8277f
elribonazo <elribonazo@gmail.com>
2014-10-18 15:00:09 +0200

    OK

--- test/test.js
+++ test/test.js
@@ -1,9 +1,12 @@
 var spm3 = require("../lib/spotify_m3");
 
 
-
-spm3.get_album_tracks_by_artist("wegfdhgjhkj", function(err, data){
+spm3.get_album_tracks_by_artist("coldplay", function(err, data){
 	if(err) throw err;
 	console.log("DATA : ");
-	console.log(data.length);
 });
+
+
+spm3.get_album_tracks_by_artist.on("status", function(data){
+	console.log(data);
+})
